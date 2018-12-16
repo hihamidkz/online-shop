9f06ed2e24c6c3a537b3a9e8576bb8d1b0bc6d54
Nikolai Grin <Nikolai_Grin@Mail.Ru>
2018-12-16 12:11:18 +0700

    Changing carts_controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_16_040126) do
+ActiveRecord::Schema.define(version: 2018_12_16_042732) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2018_12_16_040126) do
     t.integer "cart_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity", default: 1
     t.index ["book_id"], name: "index_line_items_on_book_id"
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
   end
