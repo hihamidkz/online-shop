5e9d8de93d967c6838093af8f4b2b202ae4d321d
hamid <hihamidkz@inbox.ru>
2019-01-10 20:28:52 +0700

    Edited validations of book

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,11 +4,11 @@ class Book < ApplicationRecord
   serialize :image, JSON
   belongs_to :user, optional: true
   has_many :line_items
-  validates :title, :author, :price, presence: true
+  validates :title, :author, :price, :year, presence: true
   validates :description, length: { maximum: 1000, too_long: "Максимум %{count} символов" }
   validates :title, length: { maximum: 30, too_long: "Максимум %{count} символов" }
   validates :price, length: { maximum: 7 }
-  validates :year, numericality: { only_integer: true }, length: { maximum: 4, minimum: 4 }
+  validates :year, length: { maximum: 4, minimum: 4 }
 
   private
 
